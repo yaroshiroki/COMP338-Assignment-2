e7f0116e2ef5731ad94bdeee4fd295a8f2720f1f
Yaro Shiroki <40599664+yaroshiroki@users.noreply.github.com>
2021-12-15 19:53:15 +0000

    part 1 almost complete

---
+++ assignment 2.py
@@ -0,0 +1,41 @@
+import torch
+import numpy as np
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from torch.optim import Adam, SGD
+
+
+
+class Convolutional_Neural_Network(nn.Module):
+    def __init__(self, in_channels = 3, num_classes = 10):
+        super(Convolutional_Neural_Network, self).__init__()
+
+        #now the hidden layers
+        #hidden layer 1
+        self.convolutional_layer1 = nn.Conv2d(kernel_size = [7, 7], stride = 2, padding = 3, in_channels = 3, out_channels = 64)
+
+        #hidden layer 2
+        self.convolutional_layer2 = nn.MaxPool2d(kernel_size = [3, 3], stride = 2, padding = 0)
+
+        #hidden layer 3
+        self.convolutional_layer3 = nn.Conv2d(kernel_size = [3, 3], stride = 1, padding = 1, in_channels = 3, out_channels = 64)
+
+        #relu layer
+        self.relu = nn.ReLU(inplace=True)
+
+        #batch normalization layer
+        self.batch = nn.BatchNorm2d(3)
+        
+        #hidden layer 4
+        self.convolutional_layer4 = nn.MaxPool2d(kernel_size = [3, 3], stride = 2, padding = 0)
+
+        #hidden layer 5
+        self.fully_connected_layer = nn.Linear(in_features = 5, out_features = 5)
+
+        #softmax layer
+        self.softmax = nn.Softmax(dim=None)
+
+cnn = Convolutional_Neural_Network()
+print(cnn)
